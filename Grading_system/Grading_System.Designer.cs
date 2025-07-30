685f0573ea90cdcd0c3d118e7528161dceef9c1e
Diaz Genesis G. <genesis.diaz@jmc.edu.ph>
2025-07-30 17:16:08 +0800

    Add files via upload

---
+++ Grading_system/Grading_System.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Grading_system
+{
+    partial class Grading_System
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Grading_System));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtFullname = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.txtPrilim = new System.Windows.Forms.TextBox();
+            this.txtMidTerm = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.txtRemarks = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompute.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnCompute.Location = new System.Drawing.Point(328, 483);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(212, 50);
+            this.btnCompute.TabIndex = 0;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(62, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(62, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Course:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(62, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Prilim:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(52, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 27);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "MidTerm:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(62, 374);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 27);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(62, 416);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 27);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Final:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.Window;
+            this.label10.Location = new System.Drawing.Point(350, 40);
+            this.label10.MaximumSize = new System.Drawing.Size(889, 800);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(285, 37);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "GRADING SYSTEM";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.Window;
+            this.label11.Location = new System.Drawing.Point(52, 705);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(96, 27);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Remarks:";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.Window;
+            this.label12.Location = new System.Drawing.Point(59, 640);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(88, 27);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Average:";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // txtFullname
+            // 
+            this.txtFullname.AutoSize = true;
+            this.txtFullname.Font = new System.Drawing.Font("Impact", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFullname.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtFullname.Location = new System.Drawing.Point(193, 584);
+            this.txtFullname.Name = "txtFullname";
+            this.txtFullname.Size = new System.Drawing.Size(76, 27);
+            this.txtFullname.TabIndex = 13;
+            this.txtFullname.Text = "Course";
+            this.txtFullname.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.Text = "notifyIcon2";
+            this.notifyIcon2.Visible = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(162, 119);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(565, 26);
+            this.txtName.TabIndex = 14;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(162, 173);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(565, 26);
+            this.txtCourse.TabIndex = 15;
+            // 
+            // txtPrilim
+            // 
+            this.txtPrilim.Location = new System.Drawing.Point(162, 297);
+            this.txtPrilim.Name = "txtPrilim";
+            this.txtPrilim.Size = new System.Drawing.Size(565, 26);
+            this.txtPrilim.TabIndex = 16;
+            // 
+            // txtMidTerm
+            // 
+            this.txtMidTerm.Location = new System.Drawing.Point(163, 358);
+            this.txtMidTerm.Name = "txtMidTerm";
+            this.txtMidTerm.Size = new System.Drawing.Size(565, 26);
+            this.txtMidTerm.TabIndex = 17;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(163, 417);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(565, 26);
+            this.txtFinal.TabIndex = 18;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(174, 630);
+            this.txtAverage.Multiline = true;
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.Size = new System.Drawing.Size(276, 36);
+            this.txtAverage.TabIndex = 19;
+            // 
+            // txtRemarks
+            // 
+            this.txtRemarks.Location = new System.Drawing.Point(174, 694);
+            this.txtRemarks.Multiline = true;
+            this.txtRemarks.Name = "txtRemarks";
+            this.txtRemarks.Size = new System.Drawing.Size(276, 38);
+            this.txtRemarks.TabIndex = 20;
+            // 
+            // Grading_System
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(866, 791);
+            this.Controls.Add(this.txtRemarks);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtMidTerm);
+            this.Controls.Add(this.txtPrilim);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtFullname);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCompute);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Grading_System";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Grading_System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label txtFullname;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.TextBox txtPrilim;
+        private System.Windows.Forms.TextBox txtMidTerm;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.TextBox txtRemarks;
+    }
+}
+
---
+++ Grading_system/Grading_System.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+
+
+namespace Grading_system
+{
+
+
+
+    public partial class Grading_System : Form
+    {
+        public Grading_System()
+        {
+            InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Parse grades 
+            double prelim = double.TryParse(txtPrilim.Text, out double p) ? p : 0;
+            double midterm = double.TryParse(txtMidTerm.Text, out double m) ? m : 0;
+            double finals = double.TryParse(txtFinal.Text, out double f) ? f : 0;
+
+            // Calculate average
+            double average = (prelim + midterm + finals) / 3;
+            txtAverage.Text = average.ToString("F2");
+
+            // Determine remarks (pass if average >= 75)
+            bool isPassed = average >= 75;
+            txtRemarks.Text = isPassed ? "Passed" : "Failed";
+
+            // Color the average textbox
+            txtAverage.BackColor = isPassed ? Color.LightGreen : Color.LightCoral;
+
+            // Display course and fullname
+            txtFullname.Text = $"Name: {txtName.Text}         Course: {txtCourse.Text} ";
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label8_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            // Parse the grades from the text boxes
+            double prelim = double.TryParse(txtPrilim.Text, out double p) ? p : 0;
+            double midterm = double.TryParse(txtMidTerm.Text, out double m) ? m : 0;
+            double finals = double.TryParse(txtFinal.Text, out double f) ? f : 0;
+                
+            // Calculate the average
+            double average = (prelim + midterm + finals) / 3;
+
+            // Display the result
+            txtAverage.Text = average.ToString("F2"); // Shows 2 decimal places
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
